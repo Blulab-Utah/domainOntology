c2932caf2a4c581d35895baabea900dcc45fb407
brokenjade3000 <brochor3000@gmail.com>
2017-03-20 20:52:26 -0600

    update testAPI java

--- src/test/java/edu/utah/blulab/domainontology/testAPI.java
+++ src/test/java/edu/utah/blulab/domainontology/testAPI.java
@@ -4,56 +4,56 @@ package edu.utah.blulab.domainontology;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
 
 import org.junit.Test;
 
-import java.lang.reflect.Method;
-import java.util.ArrayList;
 
 public class testAPI {
 
 
-	public static void main(String[] args) throws Exception {
-		DomainOntology domain = new DomainOntology("/Users/melissa/git/useCases/colonoscopyQuality.owl", false);
-		//DomainOntology domain = new DomainOntology("/Users/mtharp/use_cases/DomainOntologies/pneumonia.owl");
-		//DomainOntology domain = new DomainOntology("/Users/mtharp/Desktop/vincipneu.owl.xml");
-		//DomainOntology domain = new DomainOntology("C:\\Users\\Bill\\Desktop\\carotid stenosis.owl"); 
-		//DomainOntology domain = new DomainOntology("DomainOntologyAPI/src/main/resources/colonoscopy_20141001.owl");
-		//DomainOntology domain = new DomainOntology("src/main/resources/colonoscopy_20141001.owl");
-		//domain.getVariable("leukocytosis");
-		//domain.getVariable("KA247");
-		
-		ArrayList<Variable> domainVariables = domain.getAllVariables();
-		System.out.println("********** Domain Variables: **********");
-		for(Variable var : domainVariables){
-			System.out.println(var.toString());
-		}
-		
-		/**System.out.println("********** Modifier Dictionary: **********");
-		ArrayList<Modifier> modifierDictionary = domain.createModifierDictionary();
-		for(Modifier modifier : modifierDictionary){
-			System.out.println(modifier.toString());
-		}**/
+    public static void main(String[] args) throws Exception {
+        DomainOntology domain = new DomainOntology("/Users/melissa/git/useCases/colonoscopyQuality.owl", false);
+        //DomainOntology domain = new DomainOntology("/Users/mtharp/use_cases/DomainOntologies/pneumonia.owl");
+        //DomainOntology domain = new DomainOntology("/Users/mtharp/Desktop/vincipneu.owl.xml");
+        //DomainOntology domain = new DomainOntology("C:\\Users\\Bill\\Desktop\\carotid stenosis.owl");
+        //DomainOntology domain = new DomainOntology("DomainOntologyAPI/src/main/resources/colonoscopy_20141001.owl");
+        //DomainOntology domain = new DomainOntology("src/main/resources/colonoscopy_20141001.owl");
+        //domain.getVariable("leukocytosis");
+        //domain.getVariable("KA247");
 
-		System.out.println("********** Modifier Map: **********");
-		HashMap<String, ArrayList<Modifier>> modifierMap = domain.createModifierTypeMap();
-		Iterator iterator = modifierMap.entrySet().iterator();
-		while (iterator.hasNext()){
-			Map.Entry<String, ArrayList<Modifier>> modifierEntry =
-					(Map.Entry<String, ArrayList<Modifier>>)iterator.next();
-			System.out.print(modifierEntry.getKey() + ":\t");
-			for(Modifier modifier : modifierEntry.getValue()){
-				System.out.print(modifier.getModName() + "  ");
-			}
-			System.out.println("");
-		}
-		
-		/**System.out.println("********** Target Dictionary: **********");
-		ArrayList<Anchor> targetDictionary = domain.createAnchorDictionary();
-		for(Anchor target : targetDictionary){
-			System.out.println(target.toString());
-		}**/
-	}
+        ArrayList<Variable> domainVariables = domain.getAllVariables();
+        System.out.println("********** Domain Variables: **********");
+        for (Variable var : domainVariables) {
+            System.out.println(var.toString());
+        }
+
+        /**System.out.println("********** Modifier Dictionary: **********");
+         ArrayList<Modifier> modifierDictionary = domain.createModifierDictionary();
+         for(Modifier modifier : modifierDictionary){
+         System.out.println(modifier.toString());
+         }**/
+
+        System.out.println("********** Modifier Map: **********");
+        HashMap<String, ArrayList<Modifier>> modifierMap = domain.createModifierTypeMap();
+        Iterator iterator = modifierMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, ArrayList<Modifier>> modifierEntry =
+                    (Map.Entry<String, ArrayList<Modifier>>) iterator.next();
+            System.out.print(modifierEntry.getKey() + ":\t");
+            for (Modifier modifier : modifierEntry.getValue()) {
+                System.out.print(modifier.getModName() + "  ");
+            }
+            System.out.println("");
+        }
+
+        /**System.out.println("********** Target Dictionary: **********");
+         ArrayList<Anchor> targetDictionary = domain.createAnchorDictionary();
+         for(Anchor target : targetDictionary){
+         System.out.println(target.toString());
+         }**/
+    }
 
     @Test
     public void example1() throws Exception {
@@ -88,6 +88,7 @@ public class testAPI {
 
     /**
      * Use reflection to test methods
+     *
      * @throws Exception
      */
     @Test
@@ -127,37 +128,37 @@ public class testAPI {
         System.out.println("domain's methods");
         ArrayList<Variable> domainVariables = domain.getAllVariables();
         for (Variable var : domainVariables) {
-            System.out.println("Variable Name:"+var.getVarName());
+            System.out.println("Variable Name:" + var.getVarName());
             ArrayList<LogicExpression<Term>> logicExpressions = var.getAnchor();
-            for (LogicExpression<Term> logicExpression:logicExpressions){
-                if (logicExpression.isSingleExpression()){
-                    for(Term term:logicExpression){
+            for (LogicExpression<Term> logicExpression : logicExpressions) {
+                if (logicExpression.isSingleExpression()) {
+                    for (Term term : logicExpression) {
                         String targetTypeName = var.getVarName();
                         targetTypeName = targetTypeName.replaceAll(" +", "_").toUpperCase();
                         if (term.getSynonym().size() > 0) {
                             System.out.println("\tSynonyms:");
                             for (String s : term.getSynonym()) {
-                                System.out.println("\t\t"+s);
+                                System.out.println("\t\t" + s);
                             }
                         }
                         System.out.println("\tAbbreviation:");
                         if (term.getAbbreviation().size() > 0) {
                             for (String s : term.getAbbreviation())
-                                System.out.println("\t\t"+s);
+                                System.out.println("\t\t" + s);
                         }
                         System.out.println("\tMisspelling:");
                         if (term.getMisspelling().size() > 0) {
                             for (String s : term.getMisspelling())
-                                System.out.println("\t\t"+s);
+                                System.out.println("\t\t" + s);
                         }
-                        ArrayList<LogicExpression<Modifier>> modifiers=var.getModifiers();
-                        for(LogicExpression<Modifier>modifier:modifiers){
-                            System.out.println("\tModifier type: "+modifier.getType()+"\t"+modifier.isSingleExpression());
-                            for(Modifier ele:modifier){
-                                System.out.println("\tModifier name: "+ele.getModName());
-                                System.out.println("\t\tModifier get pseudo "+ele.getPseudos());
-                                System.out.println("\t\tModifier direct children: "+ele.getDirectChildren());
-                                System.out.println("\t\tIs default? "+ele.isDefault());
+                        ArrayList<LogicExpression<Modifier>> modifiers = var.getModifiers();
+                        for (LogicExpression<Modifier> modifier : modifiers) {
+                            System.out.println("\tModifier type: " + modifier.getType() + "\t" + modifier.isSingleExpression());
+                            for (Modifier ele : modifier) {
+                                System.out.println("\tModifier name: " + ele.getModName());
+                                System.out.println("\t\tModifier get pseudo " + ele.getPseudos());
+                                System.out.println("\t\tModifier direct children: " + ele.getDirectChildren());
+                                System.out.println("\t\tIs default? " + ele.isDefault());
                             }
                         }
 
@@ -166,40 +167,79 @@ public class testAPI {
             }
         }
     }
+
     @Test
     public void example4() throws Exception {
         DomainOntology domain = new DomainOntology("src/main/resources/98_heartFailure.owl", true);
         System.out.println("domain's methods");
         ArrayList<Variable> domainVariables = domain.getAllVariables();
         for (Variable var : domainVariables) {
-            System.out.println("Variable Name:"+var.getVarName());
+            System.out.println("Variable Name:" + var.getVarName());
             ArrayList<LogicExpression<Term>> logicExpressions = var.getAnchor();
-            for (LogicExpression<Term> logicExpression:logicExpressions){
-                System.out.println("\tlogicExpression type: "+logicExpression.getType());
-                if (!logicExpression.isSingleExpression()){
-                    for(Term term:logicExpression){
+            for (LogicExpression<Term> logicExpression : logicExpressions) {
+                System.out.println("\tlogicExpression type: " + logicExpression.getType());
+                if (!logicExpression.isSingleExpression()) {
+                    for (Term term : logicExpression) {
                         String targetTypeName = var.getVarName();
                         targetTypeName = targetTypeName.replaceAll(" +", "_").toUpperCase();
                         if (term.getSynonym().size() > 0) {
                             System.out.println("\tSynonyms:");
                             for (String s : term.getSynonym()) {
-                                System.out.println("\t\t"+s);
+                                System.out.println("\t\t" + s);
                             }
                         }
                         System.out.println("\tAbbreviation:");
                         if (term.getAbbreviation().size() > 0) {
                             for (String s : term.getAbbreviation())
-                                System.out.println("\t\t"+s);
+                                System.out.println("\t\t" + s);
                         }
                         System.out.println("\tMisspelling:");
                         if (term.getMisspelling().size() > 0) {
                             for (String s : term.getMisspelling())
-                                System.out.println("\t\t"+s);
+                                System.out.println("\t\t" + s);
                         }
                     }
                 }
             }
         }
     }
+
+
+    /**
+     * Use reflection to test methods
+     *
+     * @throws Exception
+     */
+    @Test
+    public void testReflectionMethods() throws Exception {
+        DomainOntology domain = new DomainOntology("src/main/resources/98_heartFailure.owl", true);
+        System.out.println("domain's methods");
+        domain.getDisplayName("");
+        for (Method m : domain.getClass().getMethods()) {
+            if (m.getParameterTypes().length == 0 && !m.getName().equals("wait") && !m.getName().startsWith("notif")) {
+                System.out.print(m.getName());
+                System.out.print(":\t" + m.invoke(domain) + "\n");
+                if (m.getReturnType().equals(ArrayList.class)) {
+                    System.out.println("\tReturned list size: " + ((ArrayList) m.invoke(domain)).size());
+                }
+            } else {
+                System.out.println("Not called: " + m.getName());
+                System.out.println("Parameter length: " + m.getParameterTypes().length);
+            }
+
+        }
+
+        System.out.println("\nvariable's methods");
+        Variable var = domain.getVariable("KA127");
+        for (Method m : var.getClass().getMethods()) {
+            if (m.getParameterTypes().length == 0 && !m.getName().equals("wait") && !m.getName().startsWith("notif")) {
+                System.out.println(m.getName() + ":\t" + m.invoke(var));
+            } else {
+                System.out.println("Not called: " + m.getName());
+                System.out.println("Parameter length: " + m.getParameterTypes().length);
+            }
+        }
+    }
+
 
 }
