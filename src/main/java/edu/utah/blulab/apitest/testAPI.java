b963a22ed2b11a19626e403d3b3c98fb01321500
Jianlin Shi <jianlinshi.cn@gmail.com>
2017-05-02 14:44:17 -0600

    v3 maven central deployable

--- src/main/java/edu/utah/blulab/apitest/testAPI.java
+++ src/main/java/edu/utah/blulab/apitest/testAPI.java
@@ -1,14 +1,33 @@
+/*
+ * Copyright  2016  Department of Biomedical Informatics, University of Utah
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package edu.utah.blulab.apitest;
+
+import edu.utah.blulab.domainontology.Anchor;
+import edu.utah.blulab.domainontology.DomainOntology;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-
-import edu.utah.blulab.domainontology.*;
 
 public class testAPI {
 
 	public static void main(String[] args) throws Exception {
 		//DomainOntology domain = new DomainOntology("/Users/melissa/git/useCases/98_heartFailure.owl", false);
+		if(args.length==0)
+			args=new String[]{"src/main/resources/colonoscopy_20141001"};
 		boolean bool = false;
 		if(args[1].equalsIgnoreCase("true")){
 			bool = true;
--- src/main/java/edu/utah/blulab/domainontology/OntologyConstants.java
+++ src/main/java/edu/utah/blulab/domainontology/OntologyConstants.java
@@ -1,7 +1,20 @@
-package edu.utah.blulab.domainontology;
+/*
+ * Copyright  2016  Department of Biomedical Informatics, University of Utah
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-import org.semanticweb.owlapi.model.PrefixManager;
-import org.semanticweb.owlapi.util.DefaultPrefixManager;
+package edu.utah.blulab.domainontology;
 
 public class OntologyConstants {
 	public static final String CT_PM = "http://blulab.chpc.utah.edu/ontologies/v2/ConText.owl";
